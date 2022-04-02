3e23429448476ec8f598786926d003ed6346a49d
Aleksandr Voronovich <vsanya93@mail.ru>
2022-04-02 16:02:57 +0300

    fixed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Проверка запроса 2"))
+                .body("data", equalTo("Проверка запроса"))
         ;
     }
 }
